3b20f2284da05e635097ab1a35ad2495279827db
danieltruong <aduy1122@gmail.com>
2022-03-12 01:09:22 -0800

    Updating landing page UI

--- frontend/src/Components/Greeting/Greeting.jsx
+++ frontend/src/Components/Greeting/Greeting.jsx
@@ -6,7 +6,8 @@ import "./greeting.css";
 function Greeting() {
     return(
         <div className="greeting-box">
-        <h1 className="main-text">Save money with spark savers</h1>
+        <h1 className="main-text">Save money </h1>
+        <p className='main-text-subtitle'> with spark savers</p>
         <h2 className="sub-text">The most effective way to see costs from switching from gas to electric</h2>
         <Link className="temp-get-started" to={"/signin"}>
         Get Started
