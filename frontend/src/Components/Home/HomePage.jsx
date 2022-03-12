10f9e58b496803da50e59226f5ed3794a999ea3a
PepeTapia <jpepetapia@csu.fullerton.edu>
2022-03-11 21:46:05 -0800

    Added greeting to home page
    The greeting is an introduction to our website and has a button to allow the user to sign in. This sign in will likely change to a more formal "Get Started" concept page.

--- frontend/src/Components/Home/HomePage.jsx
+++ frontend/src/Components/Home/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./homepage.css";
 import NavBar from "../Navbar/NavBar";
+import Greeting from "../Greeting/Greeting";
 
 function HomePage() {
   return (
     <section className="homepage-container">
       <NavBar />
+      <Greeting />
     </section>
+
   );
 }
 
 export default HomePage;
+
