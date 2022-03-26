3b5aa4f676b27e75e0d7c1a5f5572f900de3b9fc
Daniel Truong <aduy1122@gmail.com>
2022-03-26 11:22:07 -0700

    Fake login

--- frontend/src/Components/Navbar/NavBar.jsx
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -5,15 +5,18 @@ import electricIcon from "../../assets/electricIcon.png";
 
 function NavBar() {
   //This is fake user data to simulate when a user succesfully logged in
-  const [user, setUser] = useState({
-    username: "John Doe",
-    avatar:
-      "https://www.pngitem.com/pimgs/m/22-223968_default-profile-picture-circle-hd-png-download.png",
-  });
+  const [user, setUser] = useState(null);
   //For dropdown menu, will use Redux to manage this later
   const [isOpened, setOpened] = useState(false);
   const openMenu = () => {
     setOpened(!isOpened);
+  };
+  const fakeSignIn = () => {
+    setUser({
+      username: "John Doe",
+      avatar:
+        "https://www.pngitem.com/pimgs/m/22-223968_default-profile-picture-circle-hd-png-download.png",
+    });
   };
   return (
     <nav className="navbar-container">
@@ -32,7 +35,9 @@ function NavBar() {
           />
           {isOpened && (
             <div className="user-menu">
-              <Link to="/profile" className="user-item">My Profile </Link>
+              <Link to="/profile" className="user-item">
+                My Profile{" "}
+              </Link>
               <p className="user-item">Settings</p>
               <p className="user-item">Favorites</p>
               <p className="user-item">Sign out</p>
@@ -41,6 +46,7 @@ function NavBar() {
         </div>
       ) : (
         <>
+          <p onClick={fakeSignIn} style={{cursor:"pointer"}}> Fake Signin</p>
           <Link className="navbar-login" to={"/signin"}>
             Sign in
           </Link>
