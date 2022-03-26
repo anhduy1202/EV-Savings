310597e6a6b1be76c2a45af9ba3d880bd63fed6c
PepeTapia <jpepetapia@csu.fullerton.edu>
2022-03-26 09:44:44 -0700

    Added routes
    Added routes to input page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Components/Home/HomePage";
 import Auth from "./Components/Auth/Auth";
-
+import InputPage from "./Components/InputPage/InputPage"
+import InputPageElectric from "./Components/InputPageElectric/InputPageElectric"
 function App() {
   return (
     <Router>
@@ -11,6 +12,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route exact path="/signin" element={<Auth/>} />
           <Route path="/signup" element={<Auth/>} />
+          <Route path="/input" element ={<InputPage/>} />
+          <Route path="/electric" element ={<InputPageElectric/>} />
+
         </Routes>
       </div>
     </Router>
--- frontend/src/Components/Greeting/Greeting.jsx
+++ frontend/src/Components/Greeting/Greeting.jsx
@@ -9,7 +9,7 @@ function Greeting() {
         <h1 className="main-text">Save money </h1>
         <p className='main-text-subtitle'> with spark savers</p>
         <h2 className="sub-text">The most effective way to see costs from switching from gas to electric</h2>
-        <Link className="temp-get-started" to={"/signin"}>
+        <Link className="temp-get-started" to={"/input"}>
         Get Started
         </Link>
         </div>
