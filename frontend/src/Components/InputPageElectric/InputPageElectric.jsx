42e19ecf3fbd4a764a81ea0e7e7c8cb7227b110e
Daniel Truong <aduy1122@gmail.com>
2022-04-16 02:01:43 -0700

    Finish input page for electric vehicle

--- frontend/src/Components/InputPageElectric/InputPageElectric.jsx
+++ frontend/src/Components/InputPageElectric/InputPageElectric.jsx
@@ -5,28 +5,59 @@ import "./InputPageElectric.css";
 function InputPageElectric() {
   /*Select will eventually turn into a <select multiple={true} value={['B', 'C']}>
    */
-  const [vehicleName, setName] = useState();
-  const [vehicleModel, setModel] = useState();
+  const [vehicleName, setName] = useState("");
+  const [vehicleModel, setModel] = useState("");
+  const [avgMPGe, setAvgMPGe] = useState(0);
+  const [yearlyMileage, setYearlyMileage] = useState(0);
+  const [electricPrice, setElectricPrice] = useState(0);
+
+  //When user click Continue, we submit the form
+  const submitInput = (e) => {
+    e.preventDefault();
+    const savedData = {
+      vehicleName: vehicleName,
+      vehicleModel: vehicleModel,
+      avgMPGe: avgMPGe,
+      yearlyMileage: yearlyMileage,
+      electricPrice: electricPrice,
+    };
+    //Check the console on web to see the data we're passing
+    console.log(savedData);
+  };
 
   return (
     <Layout>
-      <section className="inputpageelectric-container">
-        <h2 className="inputpageelectric-header">Choose your vehicle type</h2>
-        <form>
-          <DropdownSelect
-            name="Name"
-            label="inputpageelectric-name"
-            select="inputpageelectric-select"
-            options={["Tesla", "Mazda", "Toyota", "ETC"]}
-            setState={setName}
-          />
-          <DropdownSelect
-            name="Model"
-            label="inputpageelectric-model"
-            select="inputpageelectric-select"
-            options={["3", "S", "Y", "X"]}
-            setState={setModel}
-          />
+      <section>
+        <form
+          className="inputpageelectric-container"
+          onSubmit={(e) => submitInput(e)}
+        >
+          <div className="left-container">
+            <DropdownSelect
+              title="Select Vehicle Name"
+              options={["Tesla", "Mazda", "Toyota", "ETC"]}
+              setState={setName}
+            />
+            <DropdownSelect
+              title="Select Vehicle Model"
+              options={["3", "S", "Y", "X"]}
+              setState={setModel}
+            />
+          </div>
+          <div className="right-container">
+            <InputField label="Enter your average MPGe" setState={setAvgMPGe} />
+            <InputField
+              label="Enter your yearly mileage"
+              setState={setYearlyMileage}
+            />
+            <InputField
+              label="Enter current electric price"
+              setState={setElectricPrice}
+            />
+          </div>
+          <button type="submit" className="submit-input">
+            Continue
+          </button>
         </form>
       </section>
     </Layout>
@@ -34,16 +65,33 @@ function InputPageElectric() {
 }
 
 function DropdownSelect(props) {
-  const { name, label, select, options, setState } = props;
+  const { title, options, setState } = props;
   return (
-    <label className={label}>
-      Select Vehicle {name}
-      <select className={select} onChange={(e) => setState(e.target.value)}>
+    <>
+      <label className="inputpageelectric-dropdown">{title}</label>
+      <select
+        className="inputpageelectric-select"
+        onChange={(e) => setState(e.target.value)}
+      >
         {options.map((option) => {
           return <option value={option}> {option}</option>;
         })}
       </select>
-    </label>
+    </>
+  );
+}
+
+function InputField(props) {
+  const { label, setState } = props;
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        type="text"
+        className="user-input"
+        onChange={(e) => setState(e.target.value)}
+      />
+    </>
   );
 }
 
